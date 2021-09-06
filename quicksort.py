6a2ce18a410b5d65ff879e41145d503a9022ec10
Coniferish <johnjennings702@gmail.com>
2021-09-05 21:33:44 -0400

    add quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+quicksort.py : quick sort, also known as a Partition sort, 
+    which uses a "Divide and Conquer" approach.
+Time complexity:
+Worst: O(n**2)
+Best: O(nlogn)
+Space Complexity: O(n) (since we're using recursion)
+"""
+
+__author__ = "John Jennings"
+
+# standard library
+
+# 3rd party packages
+
+# local source
